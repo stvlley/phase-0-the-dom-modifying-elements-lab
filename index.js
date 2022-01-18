009c1d38f0c5488cafc6425b004e65cf66107773
stephen talley <stvlley.amzn@gmail.com>
2022-01-18 11:25:17 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+//dom is in index.html file
+//document.querySelector('main')
+const main = document.getElementById('main');
+main.remove()
+//create element h1 and assign to var newHeader
+const newHeader = document.createElement('h1');
+//assign id 'victory' to element
+newHeader.id = 'victory'
+//add content
+newHeader.textContent = 'YOUR-NAME is the champion'
+//next line not needed for test
+document.body.append(newHeader);
